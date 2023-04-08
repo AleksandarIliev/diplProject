dd0ce485b76733c6044aef581678e8d6ad02f849
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2023-04-08 14:40:24 +0300

    Update footer :)

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <>
-            <p>Tormented not by <a href="https://github.com/AleksandarIliev?tab=repositories">Aleks.M. </a>... by someone else.</p>
+            <p>Tormented not by <a href="https://github.com/AleksandarIliev?tab=repositories">Aleks.M. </a>... by someone else :) </p>
         </>
     );
 }
